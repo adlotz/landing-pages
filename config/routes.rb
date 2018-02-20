447ad3f80cda6a99b159b83682b7ee5c712a40f8
adlotz <a.lotz@hotmail.fr>
2018-02-20 20:34:22 +0100

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  get 'home/checkr', to: 'home#checkr'
+
+  get 'home/flynn' , to: 'home#flynn'
+
+  get 'home/sparks' , to: 'home#sparks'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
